38e88b0732e2b99d2ca0768c16867d363bc5ae36
rashid597 <m.rashidkhan866@gmail.com>
2025-07-28 13:22:54 +0500

    resolves a bug

--- lib/Reporter.php
+++ lib/Reporter.php
@@ -1,8 +1,17 @@
 <?php
 
-
 namespace Safepay;
 
 class Reporter extends ApiResource
 {
+    const OBJECT_NAME = "reporter";
+    const OBJECT_PATH = "reporter.api.v1.payments";
+
+    public function instanceUrl()
+    {
+        if (null === $this["token"]) {
+            return "/reporter/api/v1/payments/";
+        }
+        return parent::instanceUrl();
+    }
 }
